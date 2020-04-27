640a186d08e3a4a4ebccb309892550090b5ad475
Mohamed Elshaer <mohamed.i.elshaer@gmail.com>
2020-04-27 15:06:10 +0200

    Refactor

--- Guess Game/ViewController.swift
+++ Guess Game/ViewController.swift
@@ -17,71 +17,64 @@ class ViewController: UIViewController {
     let rangeArray = [3,6,9,15,20,25,30,35,40,45]
     var theGuesses = [Int]()
     
-    func genrerateGuess(){
+    private func genrerateGuess(){
         theGuesses.removeAll()
-        for i in 0 ... 9{
+        
+        for i in 0 ..< rangeArray.count {
             let number = Int.random(in: 0 ... rangeArray[i])
             theGuesses.append(number)
         }
+        
         print(theGuesses)
     }
     
-    func win(){
-        let alert = UIAlertController(title: "Your Luck", message: "You Are Lucky by 100% :D \n Play Again", preferredStyle: .alert)
+    private func gameEnd(number:Int){
         
-        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
+        let alert = UIAlertController(title: "Your Luck", message: "You Are Lucky by \((number + 1) * 10)% 😁 \n Play Again", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: { action in
             self.again()
         }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
-        self.present(alert, animated: true)
-    }
-    func lose(num:Int){
-        let alert = UIAlertController(title: "Your Luck", message: "You Are Lucky by \((num + 1) * 10)% :D \n Play Again", preferredStyle: .alert)
-        
-        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
-            self.again()
-        }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         
         self.present(alert, animated: true)
     }
     
-    func again(){
+    private func again(){
         theCount = 0
-        guessRange.text = "Guess A Number Between 0 - 3"
-        guessField.text = ""
+        setLabelText(guessRange: "Guess A Number Between 0 - 3")
         genrerateGuess()
+    }
+    
+    private func setLabelText(guessRange:String){
+        self.guessRange.text = guessRange
+        guessField.text = ""
     }
     
    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(tap)
+        
         guessRange.text = "Guess A Number Between 0 - 3"
+        
         genrerateGuess()
     }
     
     @objc func dismissKeyboard() {view.endEditing(true)}
 
     @IBAction func guess(_ sender: Any) {
-        if guessField.text == String(theGuesses[0]){
-            theCount = theCount + 1
-            guessRange.text = "Guess A Number Between 0 - \(rangeArray[theCount])"
-            guessField.text = ""
-        }else if guessField.text == String(theGuesses[9]){
+        if guessField.text == String(theGuesses[9]){
+            setLabelText(guessRange: "Guess A Number Between 0 - 45")
+            gameEnd(number: theCount)
             theCount = 0
-            guessRange.text = "Guess A Number Between 0 - 45"
-            guessField.text = ""
-            win()
-        }else if guessField.text == String(theGuesses[theCount]){
+        }else if guessField.text == String(theGuesses[0]) || guessField.text == String(theGuesses[theCount]){
             theCount = theCount + 1
-            guessRange.text = "Guess A Number Between 0 - \(rangeArray[theCount])"
-            guessField.text = ""
+            setLabelText(guessRange: "Guess A Number Between 0 - \(rangeArray[theCount])")
         }else{
-            lose(num: theCount)
-             again()
+            gameEnd(number: theCount)
+            again()
         }
     }
     
