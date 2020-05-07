c9868c61df919813c31a5ce1535dcb9683ad3772
Mohamed Elshaer <mohamed.i.elshaer@gmail.com>
2020-05-07 15:40:50 +0200

    add pod

---
+++ Guess Game/Helper.swift
@@ -0,0 +1,21 @@
+//
+//  Helper.swift
+//  
+//
+//  Created by Mohamed Elshaer on 5/7/20.
+//
+
+import UIKit
+
+class Helper {
+    
+    static func showAlert(view:UIViewController,title:String,message:String){
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+        
+        view.present(alert, animated: true)
+    }
+    
+}
---
+++ Pods/Target Support Files/Pods-Guess Game/Pods-Guess Game-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Guess_Game : NSObject
+@end
+@implementation PodsDummy_Pods_Guess_Game
+@end
